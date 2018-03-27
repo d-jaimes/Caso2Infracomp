487d2728d1e5d584ba9a7b3957480192515b714e
daniela jaimes <d.jaimes@uniandes.edu.co>
2018-03-27 18:12:09 -0500

    Initial commit

---
+++ src/Sockets.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Sockets extends Thread {
+    private Socket socket;
+
+    public Sockets(Socket socket)
+    {
+        this.socket=socket;
+    }
+    @Override
+    public void run()
+    {
+    try
+    {
+         BufferedReader br= new BufferedReader(new InputStreamReader(socket.getInputStream()) );
+        //OutputStream out = socket.getOutputStream();
+
+        while((br.readLine())!=null)
+        {
+            System.out.println("The client sent : "+ br.readLine());
+        }
+
+
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+    }
+}
